9009e72991ceb581e7177d6d42205df348ed9b36
Gaurav <gauravbhongade95@gmail.com>
2023-10-23 18:50:57 +0530

    Static files added

--- ERP/erpproject/erpproject/settings.py
+++ ERP/erpproject/erpproject/settings.py
@@ -138,15 +138,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    ]
+# STATIC_URL = '/static/'
+# STATIC_ROOT = 'staticfiles'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    ]
+STATIC_ROOT = 'staticfiles'
 
 AUTH_USER_MODEL = 'registration.CustomUser'  # Replace 'myapp' with the name of your app
